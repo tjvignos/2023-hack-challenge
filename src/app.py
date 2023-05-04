d8a32372eecad1074ad79895b9f50ff1e6168f61
tjvignos <113550626+tjvignos@users.noreply.github.com>
2023-05-04 17:08:01 -0400

    added authentication

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ from db import Clothing
 from db import Outfit
 from db import Tag
 from db import Asset
+import users_dao
+import datetime
 
 app = Flask(__name__)
 db_filename = "ootd.db"
@@ -41,35 +43,129 @@ def upload():
     db.session.commit()
     return success_response(asset.serialize(), 201)
 
-# user routes
-#   SignUp User (Create User)
-
-@app.route("/signup/", methods=["POST"])
-def create_user():
+# authentication method
+def extract_token(request):
     """
-    Endpoint for creating a user
+    Helper function that extracts the token from the header of a request
+    """
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, failure_response("Missing auth header", 400)
+    bearer_token = auth_header.replace("Bearer", "").strip()
+    if not bearer_token:
+        return False, failure_response("Invalid auth header", 400)
+    return True, bearer_token
+
+# base endpoint
+@app.route("/")
+def hello_world():
+    """
+    Endpoint for printing Hello World!
+    """
+    return "Hello World!"
+
+# user routes
+@app.route("/register/", methods=["POST"])
+def register_account():
+    """
+    Endpoint for registering a new user
     """
     body = json.loads(request.data)
     username = body.get("username")
     password = body.get("password")
-    # checking if username and password are present 
-    # delegated to frontend
 
-    user = User.query.filter_by(username=username).first()
-    if user is not None:
-        return failure_response("Username taken", 400)
+    if username is None or password is None:
+        return failure_response("Invalid username or password", 400)
+    
+    created, user = users_dao.create_user(username, password)
 
-    new_user = User(
-        username = username,
-        password = password
+    if not created:
+        return failure_response("User already exists", 400)
+    
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
     )
-    db.session.add(new_user)
+
+@app.route("/login/", methods=["POST"])
+def login():
+    """
+    Endpoint for logging in a user
+    """
+    body = json.loads(request.data)
+    username = body.get("username")
+    password = body.get("password")
+    
+    if username is None or password is None:
+        return failure_response("Invalid username or password", 400)
+    
+    success, user = users_dao.verify_credentials(username, password)
+
+    if not success:
+        return failure_response("Incorrect username or password", 400)
+
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
+
+@app.route("/logout/", methods=["POST"])
+def logout():
+    """
+    Endpoint for logging out a user
+    """
+    success, session_token = extract_token(request)
+    if not success:
+        return session_token
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token", 400)
+    user.session_expiration = datetime.datetime.now()
     db.session.commit()
-    return success_response(new_user.serialize(), 201)
+    return success_response({"message": "User has successfully logged out"})
 
+@app.route("/session/", methods=["POST"])
+def update_session():
+    """
+    Endpoint for updating a user's session
+    """
+    success, update_token = extract_token(request)
+    if not success:
+        return update_token
+    user = users_dao.renew_session(update_token)
+    if user is None:
+        return failure_response("Invalid update token")
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
 
-#   Login User (IsUser)
-#   Get User
+@app.route("/secret/", methods=["POST"])
+def secret_message():
+    """
+    Endpoint for verifying a session token and returning a secret message
+
+    In your project, you will use the same logic for any endpoint that needs 
+    authentication
+    """
+    success, session_token = extract_token(request)
+
+    if not success:
+        return session_token
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token", 400)
+    
+    return success_response({"message": "Wow we implemented session token!!"})
 
 # clothing routes
 #   Create Clothing
